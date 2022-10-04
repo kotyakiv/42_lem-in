48502402b3fcb0498232619b82a34357d478d4ef
ykot <karlnord@seznam.cz>
2022-10-04 10:31:38 +0300

    path collector

--- get_paths.c
+++ get_paths.c
@@ -6,24 +6,27 @@
 /*   By: ykot <ykot@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 16:52:36 by bkandemi          #+#    #+#             */
-/*   Updated: 2022/09/26 17:42:05 by ykot             ###   ########.fr       */
+/*   Updated: 2022/10/03 20:38:54 by ykot             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lem-in.h"
+#include "lem_in.h"
 
-static int	when_to_stop(int *min_num_lines, t_list **set_i,
+static int	when_to_stop(int *min_num_lines, t_list **path_set,
 						int index, t_farm *farm)
 {
 	int	num_lines;
 	int	*queue;
 
-	queue = get_numrooms(set_i, index);
+	queue = get_numrooms(path_set, index);
 	if (queue == NULL)
+	{
+		printf("aug_9 TEST\n");
 		error(farm, "Memory allocation");
+	}
 	num_lines = count_printed_lines(farm->num_ants, queue, index);
 	//printf("Min\t%d\tCur\t%d\n", *min_num_lines, num_lines);
-	//print_paths(farm, set_i, index);
+	//print_paths(farm, path_set, index);
 	if (num_lines > *min_num_lines)
 	{
 		free(queue);
@@ -45,53 +48,69 @@ static t_list **return_prev_set(t_farm *farm, t_list	**prev_set,
 	return (prev_set);
 }
 
-static t_list **return_curr_set(t_farm *farm, t_list **set_i,
+static t_list **return_curr_set(t_farm *farm, t_list **path_set,
 								int option, size_t i)
 {
 	if (option == 1)
 		farm->index.ind1 = i;
 	else
 		farm->index.ind2 = i;
-	return (set_i);
+	return (path_set);
 }
 
 static void	init_get_paths(size_t *i, int *min_num_lines, 
-					t_list ***set_i, t_list ***prev_set)
+					t_list ***path_set, t_list ***prev_set)
 {
 	*i = 0;
-	*set_i = NULL;
+	*path_set = NULL;
 	*prev_set = NULL;
 	*min_num_lines = INT_MAX;
 }
 
 t_list	**get_paths(t_farm *farm, int option)
 {
-	t_list	**set_i;
+	t_list	**path_set;
 	t_list	**prev_set;
 	size_t	i;
 	size_t	j;
+	t_list	*all_paths;
+	t_list	*temp;
 	int		min_num_lines;
 
-	init_get_paths(&i, &min_num_lines, &set_i, &prev_set);
+	all_paths = NULL;
+	init_get_paths(&i, &min_num_lines, &path_set, &prev_set);
 	if (option == 2)
-		//reset_mark(farm);
 		reset_all_flow(farm);
 	while (bfs(farm, 0))
 	{
 		update_res_flow(farm);
-		set_i = (t_list **)ft_memalloc((i + 1) * sizeof(t_list *));
+		path_set = (t_list **)ft_memalloc((i + 1) * sizeof(t_list *));
 		j = 0;
 		while (j < i + 1 && bfs_path(farm, option))
-			set_i[j++] = mark_and_save_path(farm, 2);
+		{
+			path_set[j] = mark_and_save_path(farm, 2);
+			/*for(t_list *temp = path_set[j]; temp != NULL; temp = temp->next)
+				ft_putstr((char *)(temp->content));*/
+			if (new_path(path_set[j], all_paths))
+			{
+				temp = ft_lstnew_pointer(path_set[j]);
+				ft_lstadd(&all_paths, temp);
+				print_all_paths(all_paths);
+			}
+			j++;
+		}
 		i++;
 		reset_mark(farm);
-		if (when_to_stop(&min_num_lines, set_i, i, farm))
+		if (when_to_stop(&min_num_lines, path_set, i, farm))
 		{
-			free_set_i(set_i, i);
+			print_all_paths(all_paths);
+			//free_path_set(path_set, i);
 			return (return_prev_set(farm, prev_set, option, i));
 		}
-		free_set_i(prev_set, i - 1);
-		prev_set = set_i;
+		//free_path_set(prev_set, i - 1);
+		prev_set = path_set;
 	}
-	return (return_curr_set(farm, set_i, option, i));
+	ft_printf("\n--------------------\n\n");
+	print_all_paths(all_paths);
+	return (return_curr_set(farm, path_set, option, i));
 }
--- path_collector.c
+++ path_collector.c
@@ -6,11 +6,11 @@
 /*   By: ykot <ykot@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 15:05:24 by ykot              #+#    #+#             */
-/*   Updated: 2022/09/27 16:40:38 by ykot             ###   ########.fr       */
+/*   Updated: 2022/10/03 13:30:33 by ykot             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lem-in.h"
+#include "lem_in.h"
 
 int equal_paths(t_list *first, t_list *second)
 {
@@ -20,6 +20,8 @@ int equal_paths(t_list *first, t_list *second)
             return (0);
         if (first != NULL && second == NULL)
             return (0);
+        ft_printf("%s\n", (char *)first->content);
+        ft_printf("%s\n", (char *)second->content);
         if (!ft_strequ((char *)first->content, (char *)second->content))
             return (0);
         first = first->next;
@@ -28,16 +30,16 @@ int equal_paths(t_list *first, t_list *second)
     return (1);
 }
 
-int new_path(t_list *path, t_list **all_paths)
+int new_path(t_list *path, t_list *all_paths)
 {
-    t_list  *temp;
+    t_list *temp;
 
-    temp = *all_paths;
-    while (temp)
+    while (all_paths)
     {   
+        temp = (t_list *)all_paths->content;
         if (equal_paths(path, temp))
-            return (0);
-        temp = temp->next;
+            return (0); 
+        all_paths = all_paths->next;
     }
     return (1);
 }
@@ -51,7 +53,7 @@ void    print_all_paths(t_list *all_paths)
         temp = (t_list *)all_paths->content;
         while (temp)
         {
-            ft_putstr((char *)temp->content);
+            ft_putstr((char *)(temp->content));
             ft_putstr(" > ");
             temp = temp->next;
         }
